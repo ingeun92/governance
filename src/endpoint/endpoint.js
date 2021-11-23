fc7314b63ce12586483f72e5636d72fec287e5d3
Ingeun Kim <ingeun92@naver.com>
2021-11-23 15:10:54 +0900

    add endpoint.js

--- src/endpoint/endpoint.js
+++ src/endpoint/endpoint.js
@@ -1,5 +1,5 @@
 import Web3 from "web3";
 
 export const endpoint = new Web3(
-  "https://polygon-mumbai.g.alchemy.com/v2/ZoPCKJ_IBRTaiQTO80Z-rxQgcgvTTf-y"
+  "https://polygon-mumbai.g.alchemy.com/v2/mkm4u7lz0jQA4TlWxBc1YDjksW1DbQTH"
 );
